832408226bdccfb41b7b6ef6a097023c017a8536
fadamaka <tamas.hercz@cubicfox.com>
2023-12-01 21:02:29 +0100

    reformat; minor changes; readme update

--- day_1/calc-pt2.cpp
+++ day_1/calc-pt2.cpp
@@ -1,21 +1,17 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <iterator>
-#include <fstream>
-#include <vector>
-#include <algorithm>
 #include <string>
+#include <vector>
 
 using namespace std;
-string replaceFirstNumber(string str)
-{
+string replaceFirstNumber(string str) {
     string nums[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    for (int i = 3; i <= str.length(); i++)
-    {
+    for (int i = 3; i <= str.length(); i++) {
         string subStr = str.substr(0, i);
-        for (int j = 0; j < 9; j++)
-        {
-            if (subStr.find(nums[j]) != string::npos)
-            {
+        for (int j = 0; j < 9; j++) {
+            if (subStr.find(nums[j]) != string::npos) {
                 cout << "found " << nums[j] << " at " << subStr.find(nums[j]) << endl;
                 return str.replace(subStr.find(nums[j]), nums[j].length(), to_string(j + 1));
             }
@@ -24,16 +20,12 @@ string replaceFirstNumber(string str)
     return str;
 }
 
-string replaceLastNumber(string str)
-{
+string replaceLastNumber(string str) {
     string nums[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    for (int i = str.size() - 3; i >= 0; i--)
-    {
+    for (int i = str.size() - 3; i >= 0; i--) {
         string subStr = str.substr(i, str.size() + 1);
-        for (int j = 0; j < 9; j++)
-        {
-            if (subStr.find(nums[j]) != string::npos)
-            {
+        for (int j = 0; j < 9; j++) {
+            if (subStr.find(nums[j]) != string::npos) {
                 cout << "found " << nums[j] << " at " << str.length() - subStr.length() << endl;
                 return str.replace(str.length() - subStr.length(), nums[j].length(), to_string(j + 1));
             }
@@ -42,30 +34,23 @@ string replaceLastNumber(string str)
     return str;
 }
 
-int getFirstNumber(string str)
-{
-    for (int i = 0; i < str.length(); i++)
-    {
-        if (int(str[i]) > 48 && int(str[i]) < 58)
-        {
+int getFirstNumber(string str) {
+    for (int i = 0; i < str.length(); i++) {
+        if (int(str[i]) > 48 && int(str[i]) < 58) {
             return int(str[i]) - 48;
         }
     }
     return 0;
 }
-int getLastNumber(string str)
-{
-    for (int i = str.length() - 1; i >= 0; i--)
-    {
-        if (int(str[i]) > 48 && int(str[i]) < 58)
-        {
+int getLastNumber(string str) {
+    for (int i = str.length() - 1; i >= 0; i--) {
+        if (int(str[i]) > 48 && int(str[i]) < 58) {
             return int(str[i]) - 48;
         }
     }
     return 0;
 }
-int main()
-{
+int main() {
     ifstream is("data.txt");
     istream_iterator<string> start(is), end;
     vector<string> lines(start, end);
@@ -74,8 +59,7 @@ int main()
     // print the lines to stdout
     cout << "\n lines read in:";
     int sum = 0;
-    for (string str : lines)
-    {
+    for (string str : lines) {
         cout << "\n str = " << str << endl;
 
         string fStr = replaceFirstNumber(str);
@@ -90,5 +74,6 @@ int main()
         cout << "the number is " << number << endl;
         sum += number;
     }
-    cout << "the result is:" << sum << endl;
+    cout << "the result is: " << sum << endl;
+    cout.flush();
 }
--- day_1/calc.cpp
+++ day_1/calc.cpp
@@ -1,50 +1,42 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <iterator>
-#include <fstream>
-#include <vector>
-#include <algorithm>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
+int main() {
     ifstream is("data.txt");
     istream_iterator<string> start(is), end;
     vector<string> lines(start, end);
     cout << "Read " << lines.size() << " lines" << endl;
-
-    // print the lines to stdout
     cout << "\n lines read in:";
     int sum = 0;
-    for (string str : lines)
-    {
+    for (string str : lines) {
         cout << "\n str = " << str << endl;
         bool first = true;
         int number = 0;
-        for (int i = 0; i < str.length(); i++)
-        {
-
-            if (int(str[i]) > 48 && int(str[i]) < 58 && first)
-            {
+        for (int i = 0; i < str.length(); i++) {
+            if (int(str[i]) > 48 && int(str[i]) < 58 && first) {
                 cout << str[i] << " ";
                 cout << int(str[i]) - 48 << " as num ";
                 number += (int(str[i]) - 48) * 10;
                 first = false;
+                break;
             }
         }
         bool last = true;
-        for (int i = str.length() - 1; i >= 0; i--)
-        {
-            if (int(str[i]) > 48 && int(str[i]) < 58 && last)
-            {
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (int(str[i]) > 48 && int(str[i]) < 58 && last) {
                 cout << str[i] << " ";
                 cout << int(str[i]) - 48 << " as num ";
                 number += (int(str[i]) - 48);
                 last = false;
+                break;
             }
         }
-
         cout << "the number is " << number << endl;
         sum += number;
     }
