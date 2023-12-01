56e40d76c3e2998e8bb020df81994485c2ea69f7
fadamaka <tamas.hercz@cubicfox.com>
2023-12-01 16:32:45 +0100

    day 1 very dirty first part

--- day_1/calc.cpp
+++ day_1/calc.cpp
@@ -15,8 +15,38 @@ int main()
     cout << "Read " << lines.size() << " lines" << endl;
 
     // print the lines to stdout
-    cout << "lines read in:\n";
-    copy(lines.begin(), lines.end(),
-         ostream_iterator<string>(cout, "\n "));
-    cout << endl;
+    cout << "\n lines read in:";
+    int sum = 0;
+    for (string str : lines)
+    {
+        cout << "\n str = " << str << endl;
+        bool first = true;
+        int number = 0;
+        for (int i = 0; i < str.length(); i++)
+        {
+
+            if (int(str[i]) > 48 && int(str[i]) < 58 && first)
+            {
+                cout << str[i] << " ";
+                cout << int(str[i]) - 48 << " as num ";
+                number += (int(str[i]) - 48) * 10;
+                first = false;
+            }
+        }
+        bool last = true;
+        for (int i = str.length() - 1; i >= 0; i--)
+        {
+            if (int(str[i]) > 48 && int(str[i]) < 58 && last)
+            {
+                cout << str[i] << " ";
+                cout << int(str[i]) - 48 << " as num ";
+                number += (int(str[i]) - 48);
+                last = false;
+            }
+        }
+
+        cout << "the number is " << number << endl;
+        sum += number;
+    }
+    cout << "the result is:" << sum << endl;
 }
