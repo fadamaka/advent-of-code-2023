731ecf79c2af47287b3a344b5c1436cbc997e88c
fadamaka <tamas.hercz@cubicfox.com>
2023-12-02 10:00:08 +0100

    first part done

--- day_2/part1.cpp
+++ day_2/part1.cpp
@@ -2,22 +2,77 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <map>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-int main() {
-    ifstream is("data.txt");
-    istream_iterator<string> start(is), end;
-    vector<string> lines(start, end);
-    cout << "Read " << lines.size() << " lines" << endl;
-
-    // print the lines to stdout
-    cout << "\n lines read in:";
-    int sum = 0;
-    for (string str : lines) {
-        cout << "\n str = " << str << endl;
+vector<string> customSplit(string str) {
+    int startIndex = 0, endIndex = 0;
+    vector<string> strings;
+    for (int i = 0; i <= str.size(); i++) {
+        if (str[i] == ':' || str[i] == ',' || str[i] == ';' || str[i] == ':' || i == str.size()) {
+            endIndex = i;
+            string temp;
+            temp.append(str, startIndex, endIndex - startIndex);
+            strings.push_back(temp);
+            startIndex = endIndex + 1;
+        }
     }
-    cout << "the result is: " << sum << endl;
+    return strings;
+}
+
+vector<string> splitBySpaces(string str) {
+    int startIndex = 0, endIndex = 0;
+    vector<string> strings;
+    for (int i = 0; i <= str.size(); i++) {
+        if (str[i] == ' ' || str[i] == '\n' || i == str.size()) {
+            endIndex = i;
+            string temp;
+            temp.append(str, startIndex, endIndex - startIndex);
+            if (temp == "" || temp == "\n" || temp == " ")
+                continue;
+            strings.push_back(temp);
+            startIndex = endIndex + 1;
+        }
+    }
+    return strings;
+}
+
+int main() {
+    map<string, int> rulesMap = {{"red", 12}, {"green", 13}, {"blue", 14}};
+
+    string str;
+    vector<string> lines;
+
+    fstream file;
+    file.open("data.txt", ios::in);
+
+    while (getline(file, str)) {
+        lines.push_back(str);
+    }
+    int result = 0;
+
+    for (int i = 0; i < lines.size(); i++) {
+        cout << "game number " << i + 1 << " " << lines[i] << endl;
+        vector<string> line = customSplit(lines[i]);
+        line.erase(line.begin());
+        bool tooMuch = false;
+        for (string splits : line) {
+            vector<string> values = splitBySpaces(splits);
+            int num = stoi(values[0].erase(0, 1));
+            string color = values[1];
+            cout << "num:" << num << " color: " << color << " " << rulesMap.find(color)->second << endl;
+            if (rulesMap.find(color)->second < num) {
+                tooMuch = true;
+                cout << "too much" << endl;
+            }
+        }
+        if (!tooMuch) {
+            result += (i + 1);
+        }
+        cout << endl;
+    }
+    cout << "result: " << result << endl;
 }
