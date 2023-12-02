94b55900fd5d04f72ddd21f59a4dd059c65b32ec
fadamaka <tamas.hercz@cubicfox.com>
2023-12-02 10:29:33 +0100

    part2 done

--- day_2/part2.cpp
+++ day_2/part2.cpp
@@ -55,24 +55,26 @@ int main() {
     int result = 0;
 
     for (int i = 0; i < lines.size(); i++) {
+        map<string, int> valuesMap = {{"red", 0}, {"green", 0}, {"blue", 0}};
+
         cout << "game number " << i + 1 << " " << lines[i] << endl;
         vector<string> line = customSplit(lines[i]);
         line.erase(line.begin());
-        bool tooMuch = false;
         for (string splits : line) {
             vector<string> values = splitBySpaces(splits);
             int num = stoi(values[0].erase(0, 1));
             string color = values[1];
             cout << "num:" << num << " color: " << color << " " << rulesMap.find(color)->second << endl;
-            if (rulesMap.find(color)->second < num) {
-                tooMuch = true;
-                cout << "too much" << endl;
+            if (valuesMap.find(color)->second < num) {
+                valuesMap.find(color)->second = num;
             }
         }
-        if (!tooMuch) {
-            result += (i + 1);
+        int rowResult = 1;
+        for (std::map<string, int>::iterator it = valuesMap.begin(); it != valuesMap.end(); ++it) {
+            rowResult *= it->second;
         }
-        cout << endl;
+        cout << "row result: " << rowResult << endl;
+        result += rowResult;
     }
     cout << "result: " << result << endl;
 }
