681899ae8e267e7507c447a1f6990f53684d8bf1
fadamaka <tamas.hercz@cubicfox.com>
2023-12-08 09:41:17 +0100

    day 8 pt 1 base

---
+++ day_8/part1.cpp
@@ -0,0 +1,13 @@
+#include <..\utils\aoc.h>
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<string> lines = readFileIntoVector("data.txt");
+    map<string, pair<string, string>> keyMap;
+}
