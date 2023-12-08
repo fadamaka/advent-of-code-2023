0183600899f6f0b07f89f348ac49b605f4c9052a
fadamaka <tamas.hercz@cubicfox.com>
2023-12-08 10:42:25 +0100

    day 8 pt 2 done

--- day_8/part2.cpp
+++ day_8/part2.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <map>
+#include <numeric>
 #include <string>
 #include <vector>
 
@@ -11,26 +12,50 @@ int main() {
     vector<string> lines = readFileIntoVector("data.txt");
     map<string, pair<string, string>> keyMap;
     string directions = lines[0];
+    vector<string> startingPoints;
 
     for (int i = 2; i < lines.size(); i++) {
         vector<string> key = splitByChar(lines[i], '=');
         vector<string> dests = splitByChar(key[1], ',');
         keyMap.insert(pair<string, pair<string, string>>(key[0].substr(0, 3), pair<string, string>(dests[0].substr(2, 3), dests[1].substr(1, 3))));
-    }
-    string currentPoz = "AAA";
-    int count = 0;
-    int directionPoz = 0;
-    while (currentPoz != "ZZZ") {
-        count++;
-        if (directions[directionPoz] == 'L') {
-            currentPoz = keyMap.find(currentPoz)->second.first;
-        } else {
-            currentPoz = keyMap.find(currentPoz)->second.second;
+        if (key[0].substr(0, 3)[2] == 'A') {
+            startingPoints.push_back(key[0].substr(0, 3));
         }
+    }
+    unsigned long long result = stoll("1");
+    vector<pair<int, int>> exits;
+    for (string str : startingPoints) {
+        string currentPoz = str;
+        int count = 0;
+        int directionPoz = 0;
+        int exitCount = 0;
+        int firstExit = 0;
+        int secondExit = 0;
+        while (exitCount < 2) {
+            count++;
+            if (directions[directionPoz] == 'L') {
+                currentPoz = keyMap.find(currentPoz)->second.first;
+            } else {
+                currentPoz = keyMap.find(currentPoz)->second.second;
+            }
 
-        if (++directionPoz == directions.size()) {
-            directionPoz = 0;
+            if (++directionPoz == directions.size()) {
+                directionPoz = 0;
+            }
+            if (currentPoz[2] == 'Z') {
+                if (exitCount == 0) {
+                    firstExit = count;
+                } else {
+                    secondExit = count;
+                }
+                exitCount++;
+                cout << count << " ";
+            }
         }
+        result = lcm(result, firstExit);
+        exits.push_back(pair<int, int>(firstExit, secondExit));
+        cout << " increment: " << secondExit - firstExit << endl;
     }
-    cout << count;
+
+    cout << result;
 }
