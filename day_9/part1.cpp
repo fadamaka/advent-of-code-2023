e0c9222f9a702d39264ece6066429c5da9b70042
fadamaka <tamas.hercz@cubicfox.com>
2023-12-09 08:21:00 +0100

    day 9 pt 1 done

--- day_9/part1.cpp
+++ day_9/part1.cpp
@@ -1,11 +1,42 @@
 #include <..\utils\aoc.h>
 
+#include <algorithm>
 #include <iostream>
+#include <set>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+int getExtra(vector<int> v);
+
 int main() {
     vector<string> lines = readFileIntoVector("data.txt");
+    vector<vector<int>> input;
+
+    transform(lines.begin(), lines.end(), back_inserter(input), [](const string& str) {
+        vector<int> v;
+        vector<string> sv;
+        sv = splitByChar(str, ' ');
+        transform(sv.begin(), sv.end(), back_inserter(v), [](const string& str) { return stoi(str); });
+        return v;
+    });
+    long long result = 0;
+    for (vector<int> v : input) {
+        result += getExtra(v);
+    }
+    cout << result << endl;
+}
+
+int getExtra(vector<int> v) {
+    vector<int> proc;
+    for (int i = 0; i < v.size() - 1; i++) {
+        proc.push_back(v[i + 1] - v[i]);
+    }
+    set<int> procSet(proc.begin(), proc.end());
+    if (procSet.size() == 1) {
+        return *v.rbegin() + *procSet.begin();
+    } else {
+        return getExtra(proc) + *v.rbegin();
+    }
 }
