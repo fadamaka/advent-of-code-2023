01d7826ac4304d58b29141e6fb8ec5b915ffe260
fadamaka <tamas.hercz@cubicfox.com>
2023-12-13 19:00:29 +0100

    refactored pt1

--- day_13/part1.cpp
+++ day_13/part1.cpp
@@ -11,15 +11,12 @@
 using namespace std;
 
 vector<string> transpose(vector<string> data) {
-    // this assumes that all inner vectors have the same size and
-    // allocates space for the complete result in advance
     vector<string> result;
     for (int i = 0; i < data[0].size(); i++) {
         string str;
         for (int j = 0; j < data.size(); j++) {
             str += data[j][i];
         }
-        // cout << "pos: " << str << endl;
         result.push_back(str);
     }
     return result;
@@ -51,13 +48,6 @@ int findMirror(vector<string> field, map<string, set<int>> m) {
             } else {
                 prevCandidates = candidates;
             }
-            // for (int x : dupes) {
-            //     cout << " " << x;
-            // }
-            // cout << endl;
-            // int f = *(dupes.begin());
-            // int s = *next(dupes.begin(), 1);
-
             if (prevCandidates.find(i + 2) != prevCandidates.end()) {
                 return i + 1;
             }
@@ -91,13 +81,6 @@ int findMirror(vector<string> field, map<string, set<int>> m) {
             } else {
                 prevCandidates = candidates;
             }
-            // for (int x : dupes) {
-            //     cout << " " << x;
-            // }
-            // cout << endl;
-            // int f = *(dupes.begin());
-            // int s = *next(dupes.begin(), 1);
-
             if (prevCandidates.find(i) != prevCandidates.end()) {
                 return i;
             }
@@ -106,6 +89,25 @@ int findMirror(vector<string> field, map<string, set<int>> m) {
         }
     }
     return 0;
+}
+map<string, set<int>> generateMap(vector<string> field) {
+    map<string, set<int>> m;
+    for (int i = 0; i < field.size(); i++) {
+        if (m.count(field[i]) == 0) {
+            m.insert(pair<string, set<int>>(field[i], {i + 1}));
+        } else {
+            m.find(field[i])->second.insert(i + 1);
+        }
+    }
+    return m;
+}
+vector<map<string, set<int>>> generateMapVector(vector<vector<string>> parsedInput) {
+    vector<map<string, set<int>>> maps;
+    for (int j = 0; j < parsedInput.size(); j++) {
+        vector<string> field = parsedInput[j];
+        maps.push_back(generateMap(field));
+    }
+    return maps;
 }
 
 int main() {
@@ -125,101 +127,27 @@ int main() {
     }
 
     // vertical segment
-    vector<map<string, set<int>>> vertMaps;
+    vector<map<string, set<int>>> vertMaps = generateMapVector(parsedInput);
 
-    for (int j = 0; j < parsedInput.size(); j++) {
-        vector<string> field = parsedInput[j];
-        vertMaps.push_back(map<string, set<int>>());
-        for (int i = 0; i < field.size(); i++) {
-            // cout << field[i] << endl;
-            if (vertMaps[j].count(field[i]) == 0) {
-                vertMaps[j].insert(pair<string, set<int>>(field[i], {i + 1}));
-            } else {
-                vertMaps[j].find(field[i])->second.insert(i + 1);
-            }
-        }
-        // cout << endl;
-    }
-
-    // for (map<string, set<int>> m : vertMaps) {
-    //     cout << endl;
-    //     for (pair<string, set<int>> p : m) {
-    //         cout << p.first;
-    //         for (int i : p.second) {
-    //             cout << " " << i;
-    //         }
-
-    //         cout << endl;
-    //     }
-    // }
     int result = 0;
 
     // horizontal segment
     vector<vector<string>> pivotedInput;
     transform(parsedInput.begin(), parsedInput.end(), back_inserter(pivotedInput), [](const vector<string>& v) { return transpose(v); });
 
-    vector<map<string, set<int>>> horMaps;
-
-    for (int j = 0; j < pivotedInput.size(); j++) {
-        vector<string> field = pivotedInput[j];
-        horMaps.push_back(map<string, set<int>>());
-        for (int i = 0; i < field.size(); i++) {
-            // cout << field[i] << endl;
-            if (horMaps[j].count(field[i]) == 0) {
-                horMaps[j].insert(pair<string, set<int>>(field[i], {i + 1}));
-            } else {
-                horMaps[j].find(field[i])->second.insert(i + 1);
-            }
-        }
-        // cout << endl;
-    }
-
-    // int debug = 57;
-
-    // for (string str : parsedInput[debug]) {
-    //     cout << str << endl;
-    // }
-    // cout << endl;
-    // for (pair<string, set<int>> p : vertMaps[debug]) {
-    //     cout << p.first;
-    //     for (int i : p.second) {
-    //         cout << " " << i;
-    //     }
-
-    //     cout << endl;
-    // }
-
-    // cout << findMirror(parsedInput[debug], vertMaps[debug]);
-
-    // cout << endl;
-    // for (string str : pivotedInput[debug]) {
-    //     cout << str << endl;
-    // }
-    // cout << endl;
-    // for (pair<string, set<int>> p : horMaps[debug]) {
-    //     cout << p.first;
-    //     for (int i : p.second) {
-    //         cout << " " << i;
-    //     }
-
-    //     cout << endl;
-    // }
-
-    // cout << findMirror(pivotedInput[debug], horMaps[debug]);
-
-    // return 0;
+    vector<map<string, set<int>>> horMaps = generateMapVector(pivotedInput);
 
     for (size_t i = 0; i < vertMaps.size(); i++) {
         map<string, set<int>> m = vertMaps[i];
         vector<string> strV = parsedInput[i];
         int mirrorpoz = findMirror(strV, m);
-        cout << i + 1 << ": horizontal mirror poz: " << mirrorpoz << endl;
+        // cout << i + 1 << ": horizontal mirror poz: " << mirrorpoz << endl;
 
         map<string, set<int>> m2 = horMaps[i];
         vector<string> strV2 = pivotedInput[i];
         int mirrorpoz2 = findMirror(strV2, m2);
 
-        cout << i + 1 << ": vertical mirror poz: " << mirrorpoz2 << endl;
+        // cout << i + 1 << ": vertical mirror poz: " << mirrorpoz2 << endl;
         if (mirrorpoz2 == 0 && mirrorpoz == 0) {
             cout << "vmi" << endl;
         }
@@ -229,6 +157,5 @@ int main() {
         result += mirrorpoz2;
         result += mirrorpoz * 100;
     }
-
-    cout << "result: " << result;
+    cout << "result: " << result << endl;
 }
