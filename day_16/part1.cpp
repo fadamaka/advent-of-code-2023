cab9619019494c57ee4c81217b918651a909a7f1
fadamaka <tamas.hercz@cubicfox.com>
2023-12-16 14:00:23 +0100

    day 16 pt1 and 2 done in same file

--- day_16/part1.cpp
+++ day_16/part1.cpp
@@ -1,27 +1,33 @@
 #include <..\utils\aoc.h>
 
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 
 using namespace std;
-
-vector<string> grid;
-
 enum direction {
     N,
     S,
     W,
-    E
+    E,
+    O
 };
+vector<string> grid;
+vector<string> heat;
+vector<vector<map<direction, int>>> loop;
 
 void move(int y, int x, direction d) {
+    if (loop[y][x][d]++ > 1) {
+        return;
+    }
     char pos = grid[y][x];
+    heat[y][x] = '#';
     direction newD;
-    direction split;
+    direction split = O;
 
     switch (pos) {
-        case '/':
+        case '\\':
             if (d == N) {
                 newD = W;
             } else if (d == E) {
@@ -34,7 +40,7 @@ void move(int y, int x, direction d) {
 
             break;
 
-        case '\\':
+        case '/':
             if (d == N) {
                 newD = E;
             } else if (d == E) {
@@ -51,6 +57,8 @@ void move(int y, int x, direction d) {
             if (d == N || d == S) {
                 newD = E;
                 split = W;
+            } else {
+                newD = d;
             }
 
             break;
@@ -59,15 +67,123 @@ void move(int y, int x, direction d) {
             if (d == E || d == W) {
                 newD = S;
                 split = N;
+            } else {
+                newD = d;
             }
 
             break;
 
         default:
+            newD = d;
             break;
+    }
+splitjump:
+    switch (newD) {
+        case N:
+            if (y > 0) {
+                move(y - 1, x, newD);
+            }
+            break;
+
+        case W:
+
+            if (x > 0) {
+                move(y, x - 1, newD);
+            }
+            break;
+        case E:
+            if (x + 1 < grid[y].size()) {
+                move(y, x + 1, newD);
+            }
+            break;
+
+        case S:
+
+            if (y + 1 < grid.size()) {
+                move(y + 1, x, newD);
+            }
+            break;
+        default:
+            break;
+    }
+    if (split != O) {
+        newD = split;
+        split = O;
+        goto splitjump;
     }
 }
 
+void reset() {
+    heat.clear();
+    loop.clear();
+    for (int i = 0; i < grid.size(); i++) {
+        string str;
+        vector<map<direction, int>> v;
+        for (int j = 0; j < grid[i].size(); j++) {
+            map<direction, int> m;
+            str += '.';
+            m[N] = 0;
+            m[S] = 0;
+            m[E] = 0;
+            m[W] = 0;
+            v.push_back(m);
+        }
+        heat.push_back(str);
+        loop.push_back(v);
+    }
+}
+
+int count() {
+    int result = 0;
+    for (int i = 0; i < grid.size(); i++) {
+        for (int j = 0; j < grid[i].size(); j++) {
+            // cout << heat[i][j];
+            if (heat[i][j] == '#') {
+                result++;
+            }
+        }
+        // cout << endl;
+    }
+    return result;
+}
 int main() {
     grid = readFileIntoVector("data.txt");
+    reset();
+    move(0, 0, E);
+    int result = count();
+    cout << "part1: " << result << endl;
+    int newResult = 0;
+    for (size_t i = 0; i < grid[0].size(); i++) {
+        reset();
+        move(0, i, S);
+        newResult = count();
+        if (newResult > result) {
+            result = newResult;
+        }
+        int y = grid.size() - 1;
+        reset();
+        move(y, i, N);
+        newResult = count();
+        if (newResult > result) {
+            result = newResult;
+        }
+    }
+
+    for (size_t i = 0; i < grid.size(); i++) {
+        reset();
+        move(i, 0, E);
+        newResult = count();
+        if (newResult > result) {
+            result = newResult;
+        }
+        int y = grid[i].size() - 1;
+        reset();
+        move(i, y, W);
+        newResult = count();
+        if (newResult > result) {
+            result = newResult;
+        }
+    }
+
+    cout << "part2: " << result << endl;
 }
