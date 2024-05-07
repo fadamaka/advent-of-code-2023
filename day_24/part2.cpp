b5ba619899315ca76143edeb25ea85b941f5869b
fadamaka <tamas.hercz@cubicfox.com>
2024-05-08 00:13:39 +0200

    day 24 pt 2 dead end

--- day_24/part2.cpp
+++ day_24/part2.cpp
@@ -36,6 +36,10 @@ struct TDPoint {
         y += (yV * n);
         z += (zV * n);
     };
+    TDPoint operator-(const TDPoint& p) {
+        TDPoint difference(x, y, z, p.x - x, p.y - y, p.z - z);
+        return difference;
+    };
 };
 ostream& operator<<(ostream& os, const TDPoint& p) {
     os << p.x
@@ -56,6 +60,17 @@ int main() {
     vector<string> lines = readFileIntoVector("data.txt");
 
     TDPoint p = TDPoint(1L, 1L, 1L, 2L, 3L, -4L);
+    TDPoint p2 = TDPoint(3L, 3L, 3L, 2L, 3L, -4L);
+    TDPoint p4 = TDPoint(0L, 0L, 4L, 2L, 3L, -4L);
+    TDPoint p3 = p - p2;
+    TDPoint p5 = p4 - p2;
+    cout << "p:" << p << endl;
+    cout << "p2:" << p2 << endl;
+    cout << "p3:" << p3 << endl;
+    cout << "p4:" << p4 << endl;
+    cout << "p5:" << p5 << endl;
+    p5.increment(1);
+    cout << "p6:" << p5 << endl;
 
     cout << "before: " << p << endl;
     p.increment(1);
@@ -99,6 +114,21 @@ int main() {
         cout << counter++ << endl;
         for (auto&& i : v) {
             cout << i.second << endl;
+        }
+    }
+
+    for (auto&& i : timeVector[1]) {
+        for (auto&& j : timeVector[2]) {
+            TDPoint k = i.second - j.second;
+            k.increment(2);
+            if (timeVector[3].find(tuple<long long, long long, long long>(k.x, k.y, k.z)) != timeVector[3].end()) {
+                cout << "1: " << i.second << endl;
+                cout << "2: " << j.second << endl;
+                cout << "3: " << timeVector[3].find(tuple<long long, long long, long long>(k.x, k.y, k.z))->second << endl;
+                cout << "k: " << k << endl;
+            } else {
+                cout << "no match" << endl;
+            }
         }
     }
 }
